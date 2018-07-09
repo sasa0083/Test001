d689d505694223ce095248df656d409dab1a5419
sasa0083 <[sasa0083@outlook.kr]>
2018-07-09 17:13:54 +0900

    error

--- app.js
+++ app.js
@@ -4,3 +4,24 @@ steem.api.setOptions({url: 'https://api.steemit.com'});
 steem.api.getAccounts(['ned','dan'], function(err, response){
     console.log(err, response);
 });
+
+
+function foo() {
+  if(false) {
+    var a = 1;
+  }
+  console.log(a);
+}
+
+foo();
+
+const a = 99999;
+
+function foo1() {
+  if(false) {
+    let a = 1;
+  }
+  console.log(a);
+}
+
+foo1();
